445983d9c3ee917a69880dcfa481a2654454d5e3
Babatunde <soft-sky@live.co.uk>
2020-02-05 04:37:30 +0100

    Modify hello world app
    - [x] Add some note

--- helloWorld/HelloWorldApplication.cs
+++ helloWorld/HelloWorldApplication.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+/*
+Namespaces are heavily used in C# programming in two ways. First, .NET uses namespaces to organize its many classes. System is a namespace and Console is a class in that namespace. The using keyword can be used so that the complete name is not required,
 
+Second, declaring your own namespaces can help you control the scope of class and method names in larger programming projects.
+ */
 namespace helloWorld
 {
     class HelloWorldApplication
     {
+        // method main begins the program exection
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
